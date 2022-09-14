6d6c7ae01b5dc27074acbe14076882fa3da12e3a
“Iuliia <Iuliia_Kulagina@epam.com>
2022-09-14 10:15:00 +0200

    Fibonacci added

---
+++ L2/Fibonacci.java
@@ -0,0 +1,20 @@
+package L2;
+
+public class Fibonacci {
+    public static int Fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+         }
+    }
+    public static void main(String[] args) //распечатать n-нное число последовательности фибоначчи
+    {
+        int n = 8;
+        System.out.println(Fibonacci(n));
+
+    }
+}
+
