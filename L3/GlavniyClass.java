d387e05810a6029dff4d05fe9565ff61134d4a18
“Iuliia <Iuliia_Kulagina@epam.com>
2022-09-15 17:50:29 +0200

    With encapsilation

--- L3/GlavniyClass.java
+++ L3/GlavniyClass.java
@@ -5,12 +5,12 @@ public class GlavniyClass {
         Star Sun = new Star("Sun",332940, 1301019, null, true);
         Star SiriusA = new Star ("Sirius",660000, 2301019, null, false);
         Planet Earth = new Planet ("Earth",1, 1, "Zelenaya zelenaya trava", true);
-        Planet Neptun = new Planet ("Neptun",17, 58, null, true);
+        Planet Neptun = new Planet ("Neptun",57, 58, null, true); //Обработка некорректного
 
-        System.out.println(String.format("%s: %s земных масс, %s земных объемов, %s, %s", Sun.name, Sun.Weight, Sun.Volume, Sun.typeOfObject, Sun.well_known));
-        System.out.println(String.format("%s: %s земных масс, %s земных объемов, %s, %s", SiriusA.name, SiriusA.Weight, SiriusA.Volume, SiriusA.typeOfObject, SiriusA.well_known));
-        System.out.println(String.format("%s: %s земных масс, %s земных объемов, %s, %s", Earth.name, Earth.Weight, Earth.Volume, Earth.typeOfObject, Earth.well_known));
-        System.out.println(String.format("%s: %s земных масс, %s земных объемов, %s, %s", Neptun.name, Neptun.Weight, Neptun.Volume, Neptun.typeOfObject, Neptun.well_known));
+        System.out.println(String.format("%s: %s земных масс, %s земных объемов, тип %s, Хорошо известно = %s", Sun.getName(), Sun.getWeight(), Sun.getVolume(), Sun.typeOfObject, Sun.isWell_known()));
+        System.out.println(String.format("%s: %s земных масс, %s земных объемов, %s, %s", SiriusA.getName(), SiriusA.getWeight(), SiriusA.getVolume(), SiriusA.typeOfObject, SiriusA.isWell_known()));
+        System.out.println(String.format("%s: %s земных масс, %s земных объемов, %s, %s", Earth.getName(), Earth.getWeight(), Earth.getVolume(), Earth.typeOfObject, Earth.isWell_known()));
+        System.out.println(String.format("%s: %s земных масс, %s земных объемов, %s, %s", Neptun.getName(), Neptun.getWeight(), Neptun.getVolume(), Neptun.typeOfObject, Neptun.isWell_known()));
 
     }
 }
--- L3/SpaceObject.java
+++ L3/SpaceObject.java
@@ -4,11 +4,52 @@ public class SpaceObject {
     final public static int speed_of_light = 299792458; //м*с^-1
     final public static float gravity = 6.67430F; //(15)⋅10−11 м3·с−2·кг−1
 
-    int Volume = 0;
-    int Weight;
-    String typeOfObject;
-    boolean well_known = false;
-    String name = "AnyObject";
+    private int Volume = 0;
+    private int Weight;
+    public String typeOfObject;
+    private boolean well_known = false;
+    private String name = "AnyObject";
+
+    public int getVolume() {
+        return Volume;
+    }
+
+    public int getWeight() {
+        return Weight;
+    }
+
+    public boolean isWell_known() {
+        return well_known;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setVolume(int volume) {
+        if (Volume > 0) {
+            this.Volume = Volume;
+        } else {
+            System.out.println("incorrect Volume");
+        }
+
+    }
+
+    public void setWeight(int Weight) {
+        if (Weight < 0) {
+            this.Weight = Weight;
+        } else {
+            this.Weight = 0;
+        }
+    }
+
+        public void setWell_known(boolean well_known) {
+        this.well_known = well_known;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     protected SpaceObject(String name, int Weight, int Volume, String typeOfObject, boolean well_known) {
     System.out.println("Space object(w, v)");
